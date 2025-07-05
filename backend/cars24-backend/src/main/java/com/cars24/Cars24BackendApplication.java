42fd87baeaff125ef95e6ff5eff5090c31580d4b
Ayush Tripathi <7ayushtripathi@gmail.com>
2025-07-05 13:35:28 +0530

    backend

---
+++ backend/cars24-backend/src/main/java/com/cars24/Cars24BackendApplication.java
@@ -0,0 +1,13 @@
+package com.cars24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cars24BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cars24BackendApplication.class, args);
+    }
+
+}
